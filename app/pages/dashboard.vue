b527a7915ff9ab2f78a95c27fdfd9095b4ce4b81
Han Chee Tan <hanchee@codecultivation.com>
2026-02-04 00:57:27 +0000

    :sparkles: update sidebar for current location

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { CURRENT_LOCATION_PAGES, LOCATION_PAGES } from "~/lib/constants";
 import { useSidebarStore } from "~/stores/sidebar";
 
 const isSidebarOpen = ref(true);
@@ -7,17 +8,78 @@ const sidebarStore = useSidebarStore();
 const locationsStore = useLocationStore();
 const mapStore = useMapStore();
 
-function toggleSidebar() {
-  isSidebarOpen.value = !isSidebarOpen.value;
-  localStorage.setItem("isSidebarOpen", isSidebarOpen.value.toString());
+const { currentLocation, currentLocationStatus } = storeToRefs(locationsStore);
+
+if (LOCATION_PAGES.has(route.name?.toString() || "")) {
+  await locationsStore.refreshLocations();
+}
+
+if (CURRENT_LOCATION_PAGES.has(route.name?.toString() || "")) {
+  await locationsStore.refreshCurrentLocation();
 }
 
 onMounted(() => {
   isSidebarOpen.value = localStorage.getItem("isSidebarOpen") === "true";
-  if (route.path !== "/dashboard") {
-    locationsStore.refresh();
+});
+
+effect(() => {
+  if (LOCATION_PAGES.has(route.name?.toString() || "")) {
+    sidebarStore.sidebarTopItems = [{
+      id: "link-dashboard",
+      label: "Locations",
+      href: "/dashboard",
+      icon: "tabler:map",
+    }, {
+      id: "link-location-add",
+      label: "Add Location",
+      href: "/dashboard/add",
+      icon: "tabler:circle-plus-filled",
+    }];
+  }
+  else if (CURRENT_LOCATION_PAGES.has(route.name?.toString() || "")) {
+    sidebarStore.sidebarTopItems = [{
+      id: "link-dashboard",
+      label: "Back to Locations",
+      href: "/dashboard",
+      icon: "tabler:arrow-left",
+    }, {
+      id: "link-dashboard",
+      label: currentLocation.value ? currentLocation.value.name : "View Logs",
+      to: {
+        name: "dashboard-location-slug",
+        params: {
+          slug: currentLocation.value?.slug,
+        },
+      },
+      icon: "tabler:map",
+    }, {
+      id: "link-location-edit",
+      label: "Edit Location",
+      to: {
+        name: "dashboard-location-slug-edit",
+        params: {
+          slug: currentLocation.value?.slug,
+        },
+      },
+      icon: "tabler:map-pin-cog",
+    }, {
+      id: "link-location-add",
+      label: "Add Location Log",
+      to: {
+        name: "dashboard-location-slug-add",
+        params: {
+          slug: currentLocation.value?.slug,
+        },
+      },
+      icon: "tabler:circle-plus-filled",
+    }];
   }
 });
+
+function toggleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value;
+  localStorage.setItem("isSidebarOpen", isSidebarOpen.value.toString());
+}
 </script>
 
 <template>
@@ -41,17 +103,17 @@ onMounted(() => {
       </div>
       <div class="flex flex-col">
         <SidebarButton
+          v-for="item in sidebarStore.sidebarTopItems"
+          :key="item.id"
           :show-label="isSidebarOpen"
-          label="Locations"
-          icon="tabler:map"
-          href="/dashboard"
+          :label="item.label"
+          :icon="item.icon"
+          :href="item.href"
+          :to="item.to"
         />
-        <SidebarButton
-          :show-label="isSidebarOpen"
-          label="Add Location"
-          icon="tabler:circle-plus-filled"
-          href="/dashboard/add"
-        />
+        <div v-if="route.path.startsWith('/dashboard/location') && currentLocationStatus === 'pending'" class="flex items-center justify-center">
+          <div class="loading" />
+        </div>
         <div v-if="sidebarStore.loading || sidebarStore.sidebarItems.length" class="divider" />
         <div v-if="sidebarStore.loading" class="px-4">
           <div class="skeleton h-4 w-full" />
